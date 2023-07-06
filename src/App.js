95d5855a03992a5e0da19e76fa56038a10de6e90
SanketJadhav03 <jadhavsanket1303@gmail.com>
2023-07-06 10:24:45 +0530

    Some basic changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import Operations from "./Operations";
-// import Search from "./Search";
-// import Stories from "./Stories";
-// import Pagination from "./Pagination";
 
 const App = () => {
   return (
     <>
-    {/* <Search /> */}
-    {/* <Pagination /> */}
-    {/* <Stories /> */}
- <Operations />
+    I am App
     </>
   );
 }
