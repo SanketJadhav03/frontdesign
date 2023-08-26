34eeffdaf3628e1e76fd8bae3c08acb75ba000dd
SanketJadhav03 <jadhavsanket1303@gmail.com>
2023-08-26 08:55:39 +0530

    some minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
   return (
-    <>
-    I am App
-    </>
-  );
+    <div>
+      I am app
+    </div>
+  )
 }
-export default App;
+
+export default App
